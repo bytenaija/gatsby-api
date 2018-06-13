1c2eec9d9a61a0fec041660dcde1b6cef26adfd9
Emmanuel Nosakhare <nonreactiv@gmail.com>
2018-06-13 15:07:50 +0100

    Using babel 6

---
+++ src/models/index.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize')
+let sequelize = null
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: true //false
+  })
+} else {
+  sequelize = new Sequelize('foodgatsby', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'postgres'
+  })
+}
+
+const models = {
+  role: sequelize.import('./role'),
+  user: sequelize.import('./user'),
+  restaurant: sequelize.import('./restaurant'),
+  product: sequelize.import('./product'),
+  branch: sequelize.import('./branch'),
+  reminder: sequelize.import('./reminder'),
+  activation: sequelize.import('./activation')
+}
+
+Object.keys(models).forEach(modelName => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models)
+  }
+})
+
+models.sequelize = sequelize
+models.Sequelize = Sequelize
+
+module.exports = models
---
+++ src/server.js
@@ -0,0 +1,68 @@
+require('babel-core/register')
+require('babel-polyfill')
+
+const express = require('express')
+const logger = require('morgan')
+const UserRoutes = require('./routes/user.routes')
+const RestaurantRoutes = require('./routes/restaurant.routes')
+const AdminRoutes = require('./routes/admin.routes')
+const models = require('./models')
+
+// Set up the express app
+const app = express()
+
+// Log requests to the //console.
+app.use(logger('dev'))
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/user', UserRoutes)
+app.use('/admin', AdminRoutes)
+app.use('/restaurant', RestaurantRoutes)
+
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  )
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, content-type, Authorization, Content-Type'
+  )
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  // Pass to next layer of middleware
+  next()
+})
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+/* app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    // res.locals.message = err.message;
+    //res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    //res.status(err.status || 500);
+    res.json(err);
+}); */
+
+var port = process.env.PORT || 8000
+console.log('Port ', port)
+models.sequelize.sync().then(() => {
+  app.listen(port, () => console.log('Listening on port ' + port))
+})
