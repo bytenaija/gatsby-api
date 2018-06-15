ac0f92eee7d7fb368a4f1229840485e80ae3535d
Emmanuel Nosakhare <nonreactiv@gmail.com>
2018-06-15 23:50:23 +0100

    Added user response to /login action

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,71 +1,77 @@
-const models = require("../models");
-const User = models.user;
-const Role = models.role;
-const verify = require('../jwt/verify');
-const EmailService = require('../service/EmailService');
+const models = require('../models')
+const User = models.user
+const Role = models.role
+const verify = require('../jwt/verify')
+const EmailService = require('../service/EmailService')
 
 exports.register = (req, res, next) => {
-    console.log("Register Body ", req.body)
-    var count = 0;
-    User.findAndCountAll({ where: { email: req.body.email } }).then(user => {
-        if (user) {
-            count = user["count"];
-        }
+  console.log('Register Body ', req.body)
+  var count = 0
+  User.findAndCountAll({ where: { email: req.body.email } }).then(user => {
+    if (user) {
+      count = user['count']
+    }
 
-        if (count > 0) {
-            res.json({ "success": false, message: "This email address already exists" })
-        } else {
-
-            User.create(req.body)
-                .then(user => {
-                    var html = "<strong>Activate Your Account</strong>";
-                    // EmailService.sendMail("no-reply@bytenaija.com.ng", user.email, "Activate Your Account", html)
-                    res.json({ success: true, message: "You have successfully registered. Please proceed to login" })
-                })
-                .catch(err => {
-                    res.json({ "success": false, message: "An error occured. Please try again later", err });
-                });
-        }
-    });
-
-
+    if (count > 0) {
+      res.json({ success: false, message: 'This email address already exists' })
+    } else {
+      User.create(req.body)
+        .then(user => {
+          var html = '<strong>Activate Your Account</strong>'
+          // EmailService.sendMail("no-reply@bytenaija.com.ng", user.email, "Activate Your Account", html)
+          res.json({
+            success: true,
+            message: 'You have successfully registered. Please proceed to login'
+          })
+        })
+        .catch(err => {
+          res.json({
+            success: false,
+            message: 'An error occured. Please try again later',
+            err
+          })
+        })
+    }
+  })
 }
-
 
 exports.login = (req, res, next) => {
-    console.log("Login Body ", req.body)
-    User.findOne({ where: { email: req.body.email } }).then(user => {
-        console.log("User ", user);
-        if (user) {
-            if (user.comparePassword(req.body.password)) {
-                res.json({ token: user.getJWT() });
-            }
-        } else {
-            res.json({ "success": false, message: "Login information is incorrect" })
-        }
+  console.log('Login Body ', req.body)
+  User.findOne({ where: { email: req.body.email } }).then(user => {
+    console.log('User ', user)
+    if (user) {
+      if (user.comparePassword(req.body.password)) {
+        const { password, email, phonenumber, ...userResponse } = user
 
-    });
+        res.json({
+          token: user.getJWT(),
+          user: userResponse
+        })
+      }
+    } else {
+      res.json({ success: false, message: 'Login information is incorrect' })
+    }
+  })
 }
 
-
 exports.profile = (req, res, next) => {
-    const verification = verify.verify(req, res, next);
-    //console.log(verification);
-    if (verification) {
-        User.findOne({ where: { id: req.params.id }, include: [Role] }).then(user => {
-            res.json(user)
-        });
-    } else {
-        res.sendStatus(403);
-    }
+  const verification = verify.verify(req, res, next)
+  //console.log(verification);
+  if (verification) {
+    User.findOne({ where: { id: req.params.id }, include: [Role] }).then(
+      user => {
+        res.json(user)
+      }
+    )
+  } else {
+    res.sendStatus(403)
+  }
 }
 
 exports.allUser = (req, res, next) => {
+  //console.log(verification);
 
-    //console.log(verification);
-
-    User.findAll().then(users => {
-        res.json(users)
-    });
-
+  User.findAll().then(users => {
+    res.json(users)
+  })
 }
